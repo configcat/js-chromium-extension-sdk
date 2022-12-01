77bc142ba595e8312e990f6aa0e94b4453829544
Peter Csajtai <peter.csajtai@outlook.com>
2022-12-01 18:15:47 +0100

    Update sample

--- samples/chrome-extension/src/main.js
+++ samples/chrome-extension/src/main.js
@@ -4,16 +4,16 @@ import { LogLevel, User } from 'configcat-common';
 import * as configcat from 'configcat-js-chromium-extension'
 
 (function () {
-  async function init() {
-    // Setting log level Info to show detailed feature flag evaluation
-    const logger = configcat.createConsoleLogger(LogLevel.Info);
+  // Setting log level Info to show detailed feature flag evaluation
+  const logger = configcat.createConsoleLogger(LogLevel.Info);
 
-    // You can instantiate the client with different polling modes. See the Docs: https://configcat.com/docs/sdk-reference/js/#polling-modes
-    const configCatClient = configcat.createClientWithAutoPoll('PKDVCLf-Hq-h-kCzMp-L7Q/HhOWfwVtZ0mb30i9wi17GQ', {
-      pollIntervalSeconds: 2,
-      logger: logger
-    });
+  // You can instantiate the client with different polling modes. See the Docs: https://configcat.com/docs/sdk-reference/js/#polling-modes
+  const configCatClient = configcat.createClientWithAutoPoll('PKDVCLf-Hq-h-kCzMp-L7Q/HhOWfwVtZ0mb30i9wi17GQ', {
+    pollIntervalSeconds: 2,
+    logger: logger
+  });
 
+  function init() {
     document.getElementById('checkAwesome').addEventListener('click', () => {
       configCatClient.getValue("isAwesomeFeatureEnabled", false, function (value) {
         document.getElementById('isAwesomeEnabled').innerHTML = value;
