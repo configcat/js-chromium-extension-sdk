e85ff0fc8e4b5e98022c6f241a75ded72d3b8dc4
Peter Csajtai <peter.csajtai@outlook.com>
2022-12-01 18:10:50 +0100

    Add sample extension

---
+++ samples/chrome-extension/config/webpack.config.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+const PATHS = {
+  src: path.resolve(__dirname, '../src'),
+  build: path.resolve(__dirname, '../build'),
+};
+
+const IMAGE_TYPES = /\.(png|jpe?g|gif|svg)$/i;
+
+const config = (_env, argv) => {
+  const isProd = argv.mode === 'production'
+  return {
+    output: {
+      path: PATHS.build,
+      filename: '[name].js',
+    },
+    entry: {
+      main: PATHS.src + '/main.js',
+    },
+    stats: {
+      all: false,
+      errors: true,
+      builtAt: true,
+      assets: true,
+      excludeAssets: [IMAGE_TYPES],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: IMAGE_TYPES,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'images',
+                name: '[name].[ext]',
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: '**/*',
+            context: 'public',
+          },
+        ],
+      }),
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+      }),
+    ],
+  
+    resolve: {
+      modules: ['src', 'node_modules'],
+    },
+  
+    devtool: isProd ? false : 'inline-source-map',
+  
+    optimization: {
+      minimize: isProd,
+    },
+  };
+};
+
+module.exports = config;
---
+++ samples/chrome-extension/src/main.js
@@ -0,0 +1,34 @@
+'use strict';
+
+import { LogLevel, User } from 'configcat-common';
+import * as configcat from 'configcat-js-chromium-extension'
+
+(function () {
+  async function init() {
+    // Setting log level Info to show detailed feature flag evaluation
+    const logger = configcat.createConsoleLogger(LogLevel.Info);
+
+    // You can instantiate the client with different polling modes. See the Docs: https://configcat.com/docs/sdk-reference/js/#polling-modes
+    const configCatClient = configcat.createClientWithAutoPoll('PKDVCLf-Hq-h-kCzMp-L7Q/HhOWfwVtZ0mb30i9wi17GQ', {
+      pollIntervalSeconds: 2,
+      logger: logger
+    });
+
+    document.getElementById('checkAwesome').addEventListener('click', () => {
+      configCatClient.getValue("isAwesomeFeatureEnabled", false, function (value) {
+        document.getElementById('isAwesomeEnabled').innerHTML = value;
+      });
+    });
+
+    document.getElementById('checkProofOfConcept').addEventListener('click', () => {
+      const userEmail =  document.getElementById('userEmail').value;
+      const userObject = new User('#SOME-USER-ID#', userEmail);
+
+      configCatClient.getValue("isPOCFeatureEnabled", false, function (value) {
+        document.getElementById('isPOCEnabled').innerHTML = value;
+      }, userObject);
+    });
+  }
+
+  document.addEventListener('DOMContentLoaded', init);
+})();
