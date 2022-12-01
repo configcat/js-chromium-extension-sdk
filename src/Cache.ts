b18637a280a8b321f401452cd7171a24105f2d9f
Peter Csajtai <peter.csajtai@outlook.com>
2022-12-01 15:34:41 +0100

    Fix cache

--- src/Cache.ts
+++ src/Cache.ts
@@ -7,11 +7,16 @@ export class LocalStorageCache implements ICache {
         this.cache[key] = config;
         const obj = {};
         obj[key] = btoa(JSON.stringify(config));
-        try {
-            chrome.storage.local.set(obj);
-        } catch (ex) {
-            // chrome storage is unavailable
-        }
+        return new Promise(resolve => {
+            try {
+                chrome.storage.local.set(obj).then(() => {
+                    resolve();
+                });
+            } catch (ex) {
+                // chrome storage is unavailable
+                resolve();
+            }
+        });
     }
 
     get(key: string): Promise<ProjectConfig | null> | ProjectConfig | null {
@@ -19,22 +24,25 @@ export class LocalStorageCache implements ICache {
         if (config) {
             return config;
         }
-
-        try {
-            chrome.storage.local.get(key, (res) => {
-                const configString: string = res[key];
-                if (configString) {
-                    const config: ProjectConfig = JSON.parse(atob(configString));
-                    if (config) {
-                        this.cache[key] = config;
-                        return config;
+        return new Promise(resolve => {
+            try {
+                chrome.storage.local.get(key, (res) => {
+                    const configString: string = res[key];
+                    if (configString) {
+                        const config: ProjectConfig = JSON.parse(atob(configString));
+                        if (config) {
+                            this.cache[key] = config;
+                            resolve(config);
+                            return;
+                        }
                     }
-                }
-            });
-        } catch (ex) {
-            // chrome storage is unavailable or invalid cache value.
-        }
+                    resolve(null);
+                });
+            } catch (ex) {
+                // chrome storage is unavailable or invalid cache value.
+                resolve(null);
+            }
+        });
 
-        return null;
     }
 }
