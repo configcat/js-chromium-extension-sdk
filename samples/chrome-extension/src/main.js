8fe1af4a2f504417856425861002ef94e7b90ed9
Adam Simon <adamosimoni@gmail.com>
2023-02-17 15:29:46 +0100

    Update sample

--- samples/chrome-extension/src/main.js
+++ samples/chrome-extension/src/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import { LogLevel, User } from 'configcat-common';
+import { LogLevel, PollingMode, User } from 'configcat-js-chromium-extension';
 import * as configcat from 'configcat-js-chromium-extension'
 
 (function () {
@@ -8,14 +8,14 @@ import * as configcat from 'configcat-js-chromium-extension'
   const logger = configcat.createConsoleLogger(LogLevel.Info);
 
   // You can instantiate the client with different polling modes. See the Docs: https://configcat.com/docs/sdk-reference/js/#polling-modes
-  const configCatClient = configcat.createClientWithAutoPoll('PKDVCLf-Hq-h-kCzMp-L7Q/HhOWfwVtZ0mb30i9wi17GQ', {
+  const configCatClient = configcat.getClient('PKDVCLf-Hq-h-kCzMp-L7Q/HhOWfwVtZ0mb30i9wi17GQ', PollingMode.AutoPoll, {
     pollIntervalSeconds: 2,
     logger: logger
   });
 
   function init() {
     document.getElementById('checkAwesome').addEventListener('click', () => {
-      configCatClient.getValue("isAwesomeFeatureEnabled", false, function (value) {
+      configCatClient.getValueAsync("isAwesomeFeatureEnabled", false).then(value => {
         document.getElementById('isAwesomeEnabled').innerHTML = value;
       });
     });
@@ -24,9 +24,9 @@ import * as configcat from 'configcat-js-chromium-extension'
       const userEmail =  document.getElementById('userEmail').value;
       const userObject = new User('#SOME-USER-ID#', userEmail);
 
-      configCatClient.getValue("isPOCFeatureEnabled", false, function (value) {
+      configCatClient.getValueAsync("isPOCFeatureEnabled", false, userObject).then(value => {
         document.getElementById('isPOCEnabled').innerHTML = value;
-      }, userObject);
+      });
     });
   }
 
