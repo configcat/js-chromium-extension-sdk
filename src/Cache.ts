8e1dfe964b96644601512ddebb8e106598314e6f
adams85 <31276480+adams85@users.noreply.github.com>
2023-06-12 21:11:31 +0200

    Adjust SDK to configcat-common v8.0 (#7)
    * Remove deprecated code
    
    * Fix tests
    
    * Adjust to configcat-common v8.0

--- src/Cache.ts
+++ src/Cache.ts
@@ -1,54 +1,25 @@
-import type { ICache } from "configcat-common";
-import { ProjectConfig } from "configcat-common";
+import type { IConfigCatCache } from "configcat-common";
 
-export class LocalStorageCache implements ICache {
-  cache: { [key: string]: ProjectConfig } = {};
-
-  set(key: string, config: ProjectConfig): Promise<void> | void {
-    this.cache[key] = config;
-    const obj: any = {};
-    obj[key] = btoa(JSON.stringify(config));
-    return new Promise(resolve => {
-      try {
-        chrome.storage.local.set(obj).then(() => {
-          resolve();
-        });
-      }
-      catch (ex) {
-        // chrome storage is unavailable
-        resolve();
-      }
-    });
+export class LocalStorageCache implements IConfigCatCache {
+  async set(key: string, value: string): Promise<void> {
+    try {
+      await chrome.storage.local.set({ [key]: btoa(value) });
+    }
+    catch (ex) {
+      // chrome storage is unavailable
+    }
   }
 
-  get(key: string): Promise<ProjectConfig | null> | ProjectConfig | null {
-    const config: ProjectConfig = this.cache[key];
-    if (config) {
-      return config;
+  async get(key: string): Promise<string | undefined> {
+    try {
+      const cacheObj = await chrome.storage.local.get(key);
+      const configString = cacheObj[key];
+      if (configString) {
+        return atob(configString);
+      }
     }
-    return new Promise(resolve => {
-      try {
-        chrome.storage.local.get(key, (res) => {
-          const configString: string = res[key];
-          if (configString) {
-            const config: ProjectConfig = JSON.parse(atob(configString));
-            // JSON.parse creates a plain object instance, so we need to manually restore the prototype
-            // (so we don't run into "... is not a function" errors).
-            (Object.setPrototypeOf || ((o, proto) => o["__proto__"] = proto))(config, ProjectConfig.prototype);
-
-            if (config) {
-              this.cache[key] = config;
-              resolve(config);
-              return;
-            }
-          }
-          resolve(null);
-        });
-      }
-      catch (ex) {
-        // chrome storage is unavailable or invalid cache value.
-        resolve(null);
-      }
-    });
+    catch (ex) {
+      // chrome storage is unavailable or invalid cache value.
+    }
   }
 }
