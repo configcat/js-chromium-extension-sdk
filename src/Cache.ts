e6510c98a8443d6fa4b7bdca54cceea9e7222bf3
Adam Simon <adamosimoni@gmail.com>
2024-01-08 17:26:20 +0100

    Improve base64 decode perf. + add tests + fix code formatting

--- src/Cache.ts
+++ src/Cache.ts
@@ -3,7 +3,7 @@ import type { IConfigCatCache } from "configcat-common";
 export class LocalStorageCache implements IConfigCatCache {
   async set(key: string, value: string): Promise<void> {
     try {
-      await chrome.storage.local.set({ [key]: this.b64EncodeUnicode(value) });
+      await chrome.storage.local.set({ [key]: toUtf8Base64(value) });
     }
     catch (ex) {
       // chrome storage is unavailable
@@ -15,23 +15,23 @@ export class LocalStorageCache implements IConfigCatCache {
       const cacheObj = await chrome.storage.local.get(key);
       const configString = cacheObj[key];
       if (configString) {
-        return this.b64DecodeUnicode(configString);
+        return fromUtf8Base64(configString);
       }
     }
     catch (ex) {
       // chrome storage is unavailable or invalid cache value.
     }
   }
+}
 
-  private b64EncodeUnicode(str: string): string {
-    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function (_, p1) {
-      return String.fromCharCode(parseInt(p1, 16))
-    }));
-  }
+export function toUtf8Base64(str: string): string {
+  str = encodeURIComponent(str);
+  str = str.replace(/%([0-9A-F]{2})/g, (_, p1) => String.fromCharCode(parseInt(p1, 16)));
+  return btoa(str);
+}
 
-  private b64DecodeUnicode(str: string): string {
-    return decodeURIComponent(Array.prototype.map.call(atob(str), function (c: string) {
-      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
-    }).join(''));
-  }
+export function fromUtf8Base64(str: string): string {
+  str = atob(str);
+  str = str.replace(/[%\x80-\xFF]/g, m => "%" + m.charCodeAt(0).toString(16));
+  return decodeURIComponent(str);
 }
